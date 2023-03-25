5fd2160c4f6a7940f90e27e8ea519df2da2d6b31
Thomas <12449668+indifferentghost@users.noreply.github.com>
2023-03-24 22:00:43 -0700

    update lume add a sitemap

--- _config.ts
+++ _config.ts
@@ -1,11 +1,13 @@
 import lume from "lume/mod.ts";
 import jsx from "lume/plugins/jsx_preact.ts";
+import sitemap from "lume/plugins/sitemap.ts";
 
 const search = { returnPageData: true };
 const site = lume({}, { search });
 
 site
   .copy("styles")
-  .use(jsx());
+  .use(jsx())
+  .use(sitemap({query:"draft!=true"}))
 
 export default site;
